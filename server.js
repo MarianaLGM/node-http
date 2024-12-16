cb1327012c5dd310da78620f4fe97847a483fb4e
MarianaLGM <mariana_lobeto@hotmail.com>
2024-12-16 19:09:24 +0100

    cambios

--- server.js
+++ server.js
@@ -23,10 +23,15 @@ console.log(web);
 /*3.Configurar el servidor para responder con un documento HTML. Puedes utilizar el método `res.end()` 
  para escribir dentro tu HTML (con su !DOCTYPE, H1, H2... lo que creas que es necesario)con la datos de `data.js`*/
 
+ //PENDIENTE
  //res.end ("<h1></h1>") 
  //res.end ("<h2></h2>")
  //res.end ("<p></p>")
 
 
+
  /*4.Ejecutar el servidor y asegurarse de que responde correctamente con un documento HTML 
 que incorpora la información de la página web. */
+
+
+//PENDIENTE
